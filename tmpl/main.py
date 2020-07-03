e73f639a6426f63e091711cfb6089c285d7e63ec
ebiiim <mail@ebiiim.com>
2020-07-03 23:45:23 +0900

    tmpl

---
+++ tmpl/main.py
@@ -0,0 +1,12 @@
+from sys import stdin
+
+
+def readline_from_stdin() -> str:
+    return stdin.readline().rstrip
+
+
+if __name__ == '__main__':
+    pass
+    # N = input()
+    # X, Y, Z = input().split()
+    # A = [int(i) for i in input().split()]
