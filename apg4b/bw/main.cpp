0428ecdd4a2d2eb934bae661da934fa096ded6b6
ebiiim <mail@ebiiim.com>
2021-01-09 01:40:40 +0900

    apg4b/bw tmpl

---
+++ apg4b/bw/main.cpp
@@ -0,0 +1,16 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); ++i)
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> A(N);
+    rep(i, N) cin >> A[i];
+
+    // cout << "Hello, world!" << endl;
+}
