fa78c0ed5c013be8a6d51bb7aac80895c3c10210
ebiiim <mail@ebiiim.com>
2021-01-18 02:30:35 +0900

    tmpl

--- misc/setup/tmpl/main.cpp
+++ misc/setup/tmpl/main.cpp
@@ -3,14 +3,16 @@
 #include <bits/stdc++.h>
 #define rep(i,n) for (int i=0; i<(int)(n); ++i)
 #define rep2(i,s,n) for (int i=(s); i<(int)(n); ++i)
+#define PI 3.14159265358979323846264338327950L
 #define noflush ios::sync_with_stdio(false);cin.tie(nullptr);constexpr char endl='\n'
 #define put(expr) cout<<expr<<'\n'
 typedef int8_t i8; typedef int16_t i16; typedef int32_t i32; typedef int64_t i64;
 typedef uint8_t u8; typedef uint16_t u16; typedef uint32_t u32; typedef uint64_t u64;
+typedef float f32; typedef double f64; typedef long double f80;
 template <typename T> bool chmax(T &now, const T &cand) { if (now < cand) { now = cand; return true; } return false; }
 template <typename T> bool chmin(T &now, const T &cand) { if (now > cand) { now = cand; return true; } return false; }
 using namespace std;
-const int inf = 1<<30; /* inf > 10^9 */ const i64 infLL = 1LL<<60; /* infLL > 10^18*/
+const int INF = 1<<30; /* INF > 10^9 */ const i64 INFLL = 1LL<<60; /* INFLL > 10^18*/
 // clang-format on
 
 int main() {
@@ -22,5 +24,5 @@ int main() {
   vector<int> A(N);
   rep(i, N) cin >> A[i];
 
-  // put("hello world");
+  // put(ANS);
 }
