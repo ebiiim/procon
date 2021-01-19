e84c1b7dcdd0e2c42e86eed5aa7719194d5976e8
ebiiim <mail@ebiiim.com>
2021-01-19 14:53:42 +0900

    abc165_{a,b}

---
+++ atcoder/abc165/abc165_a/main.cpp
@@ -0,0 +1,33 @@
+// clang-format off
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i=0; i<(int)(n); ++i)
+#define rep2(i,s,n) for (int i=(s); i<(int)(n); ++i)
+#define PI 3.14159265358979323846264338327950L
+#define noflush ios::sync_with_stdio(false);cin.tie(nullptr);constexpr char endl='\n'
+#define put(expr) cout<<expr<<'\n'
+typedef int8_t i8; typedef int16_t i16; typedef int32_t i32; typedef int64_t i64;
+typedef uint8_t u8; typedef uint16_t u16; typedef uint32_t u32; typedef uint64_t u64;
+typedef float f32; typedef double f64; typedef long double f80;
+template <typename T> bool chmax(T &now, const T &cand) { if (now < cand) { now = cand; return true; } return false; }
+template <typename T> bool chmin(T &now, const T &cand) { if (now > cand) { now = cand; return true; } return false; }
+using namespace std;
+const int INF = 1<<30; /* INF > 10^9 */ const i64 INFLL = 1LL<<60; /* INFLL > 10^18*/
+// clang-format on
+
+int main() {
+  noflush;
+
+  int K;
+  cin >> K;
+
+  int A, B;
+  cin >> A >> B;
+
+  string ANS = "NG";
+
+  for (int i = A; i < B + 1; ++i) {
+    if (i % K == 0) ANS = "OK";
+  }
+  put(ANS);
+}
