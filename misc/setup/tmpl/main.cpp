6aae7b674a6b235eb50acc2eb1cb555f4885525d
ebiiim <mail@ebiiim.com>
2021-01-25 23:41:27 +0900

    tmpl

--- misc/setup/tmpl/main.cpp
+++ misc/setup/tmpl/main.cpp
@@ -25,10 +25,12 @@ using namespace std;
 #define PI 3.14159265358979323846264338327950L
 #define rep(i,n) for (int i=0; i<(int)(n); ++i)
 #define rep2(i,s,n) for (int i=(s); i<(int)(n); ++i)
-#define noflush ios::sync_with_stdio(false);cin.tie(nullptr);constexpr char endl='\n'
-#define prec(i) fixed << setprecision(i)
+#define prep_ios(do_flush) if(!do_flush){ ios::sync_with_stdio(false);cin.tie(nullptr);constexpr char endl='\n'; } cout<<fixed<<setprecision(20)
 #define put(expr) cout<<expr<<'\n'
-#define putV(A) cout << '['; rep(i, A.size()) { cout << A[i]; if (i != A.size() - 1) cout << ' '; else cout << "]\n"; }
+template<typename T> ostream &operator<<(ostream &os, const vector<T> &vec){ os << "[ "; for (auto v: vec) os << v << " "; os << "]"; return os; }template<typename T> ostream &operator<<(ostream &os, const deque<T> &vec){ os << "< "; for (auto v: vec) os << v << " "; os << ">"; return os; }
+template<typename T> ostream &operator<<(ostream &os, const set<T> &vec){ os << "{ "; for (auto v: vec) os << v << " "; os << "}"; return os; }template<typename T> ostream &operator<<(ostream &os, const unordered_set<T> &vec){ os << "{ "; for (auto v: vec) os << v << " "; os << "}"; return os; }
+template<typename TK, typename TV> ostream &operator<<(ostream &os, const map<TK, TV> &mp){ os << "{ "; for (auto v: mp) os << v.first << ": " << v.second << ", "; os << "}"; return os; }template<typename TK, typename TV> ostream &operator<<(ostream &os, const unordered_map<TK, TV> &mp){ os << "{ "; for (auto v: mp) os << v.first << ": " << v.second << ", "; os << "}"; return os; }
+template<typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &pa){ os << "(" << pa.first << ", " << pa.second << ")"; return os; }
 typedef int8_t i8; typedef int16_t i16; typedef int32_t i32; typedef int64_t i64; typedef uint8_t u8; typedef uint16_t u16; typedef uint32_t u32; typedef uint64_t u64; typedef float f32; typedef double f64; typedef long double f80;
 typedef vector<int> vi; typedef vector<vector<int>> vvi; typedef map<int, int> mii;
 typedef __int128_t i128; i128 parse(string &s) { i128 ret = 0; for (int i = 0; i < s.length(); i++) if ('0' <= s[i] && s[i] <= '9') ret = 10 * ret + s[i] - '0'; return ret; } istream &operator>>(istream &is, i128 &v) { string s; is >> s; v = parse(s); return is; } ostream &operator<<(ostream &os, const i128 &v) { if (v == 0) return (os << "0"); i128 num = v; if (v < 0) { os << '-'; num = -num; } string s; for (; num > 0; num /= 10) s.push_back('0' + (char)(num % 10)); reverse(s.begin(), s.end()); return (os << s); }
@@ -39,7 +41,7 @@ const int INF = 1<<30; /* INF > 10^9 */ const i64 INFLL = 1LL<<60; /* INFLL > 10
 // clang-format on
 
 int main() {
-  noflush;
+  prep_ios(true);
 
   int N;
   cin >> N;
@@ -47,6 +49,7 @@ int main() {
   vector<int> A(N);
   rep(i, N) cin >> A[i];
 
+  put(A);
   // string ans = YES;
   // put(ans);
 }
