990d2ddc40c477bf236d7801894733cf660c4d75
ebiiim <mail@ebiiim.com>
2021-06-16 23:33:48 +0900

    oj: fix

--- misc/setup/oj/setup-oj-tmpl.sh
+++ misc/setup/oj/setup-oj-tmpl.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -x
-if [ "$(uname)" == 'Darwin' ]; then
+if [ "$(uname)" = 'Darwin' ]; then
     echo 'darwin'
     CFG="/Users/$(whoami)/Library/Application Support/online-judge-tools"
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+elif [ "$(expr substr $(uname -s) 1 5)" = 'Linux' ]; then
     echo 'linux'
     CFG="/home/$(whoami)/.config/online-judge-tools"
 else
