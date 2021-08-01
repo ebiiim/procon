bb6b138dc5b821b42c19eb9e175d90decbad3219
ebiiim <mail@ebiiim.com>
2021-08-02 02:28:37 +0900

    tmpl: rust

--- misc/setup/tmpl/src/main.rs
+++ misc/setup/tmpl/src/main.rs
@@ -1,13 +1,17 @@
+#![allow(non_snake_case, unused_variables, unused_mut)]
+
 use proconio::input;
 
 fn main() {
     input! {
-        a: i64,
-        b: i64,
-        c: i64,
+        N: i64,
+        A: i64,
+        B: i64,
+        C: i64,
+        mut X: [i64; N],
     }
 
     let mut ans = 0;
 
-    println!("{}", ans)
+    println!("{:?}", ans)
 }
